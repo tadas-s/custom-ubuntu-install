2eef2cfe7e4dea2dfe59df6384fb6104c57eade1
Tadas Sasnauskas <tadas@yoyo.lt>
2019-03-14 14:00:31 +0000

    Drop some unnecessary clutter
    Leftovers from vagrant-centos builder which was doing bit more than this script.

--- functions.sh
+++ functions.sh
@@ -7,16 +7,6 @@ file_exists() { [[ ! -e "$1" ]] && { echo "$2"; exit 1; } }
 # Check if $1 runs and returns with 0 status
 is_runnable() { $1 >/dev/null 2>&1 || { echo "$2"; exit 1; } }
 
-# Wait until VM with name $1 powers down
-wait_vm_quit() {
-    echo "Waiting $1 to power-off"
-    while [ `vboxmanage list runningvms | grep "$1" | wc -l` == "1" ]; do
-        echo -n "."
-        sleep 5
-    done
-    echo "...and it's gone"
-}
-
 # Render template $1
 render_template() {
   eval "echo \"$(cat $1)\""
